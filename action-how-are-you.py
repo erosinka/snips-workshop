c8d009c3d1f7f03437c8fb81ebe7ad8290dd74af
elena <elena@r.com>
2019-01-27 15:55:57 +0100

    Add more intents

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -3,7 +3,8 @@
 from hermes_python.hermes import Hermes
 
 INTENT_HOW_ARE_YOU = "e_rosinska:how_are_you"
-
+INTENT_GOOD = "e_rosinska:feeling_good"
+INTENT_MOODY = "e_rosinska:feeling_moody"
 
 def main():
     with Hermes("localhost:1883") as h:
@@ -13,9 +14,17 @@ def main():
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "Amazing."
+    response = "Amazing. What about you?"
     hermes.publish_end_session(session_id, response)
 
+def feeling_good_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "Good for you."
+    hermes.publish_end_session(session_id, response)
 
+def feeling_moody_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "Want to get wasted?"
+    hermes.publish_end_session(session_id, response)
 if __name__ == "__main__":
     main()
